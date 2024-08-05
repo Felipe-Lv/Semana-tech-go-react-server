c04db0a94d6ed5964819e88919a0996541eb7d0a
Felipe Alves <fbarros.silva2000@gmail.com>
2024-08-05 19:42:44 -0400

    first commit

--- cmd/tools/terndotenv/main.go
+++ cmd/tools/terndotenv/main.go
@@ -10,6 +10,7 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		panic(err)
 	}
+	// Run tern migrate command
 
 	cmd := exec.Command("tern", "migrate", "--migrations", "./internal/store/pgstore/migrations", "--config", "./internal/store/pgstore/migrations/tern.conf")
 	if err := cmd.Run(); err != nil {
